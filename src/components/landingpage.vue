0f721d3b2dafc0c8287417780cf7d01cd64cbbfe
zaidanrafi <zaidanr4f1@gmail.com>
2022-07-23 17:40:04 +0700

    first commit

---
+++ src/components/landingpage.vue
@@ -0,0 +1,13 @@
+<template>
+  tes
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,19 +1,19 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import Home from '../views/Home.vue'
+import ecommerce from '../views/ecommerce.vue'
 
 const routes = [
   {
     path: '/',
-    name: 'Home',
-    component: Home
+    name: 'E-Commerce',
+    component: ecommerce
   },
   {
-    path: '/about',
-    name: 'About',
+    path: '/data',
+    name: 'Data',
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    component: () => import(/* webpackChunkName: "about" */ '../views/data.vue')
   }
 ]
 
